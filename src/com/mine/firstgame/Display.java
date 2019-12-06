b02a9941745988a3d20f8cf16888e93afd9c4d10
Mika-Lahtinen <image1224@outlook.com>
2019-12-06 20:33:48 +0800

    Finish episode 2

---
+++ src/com/mine/firstgame/Display.java
@@ -0,0 +1,72 @@
+package com.mine.firstgame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
+
+
+public class Display extends Canvas implements Runnable {
+
+    public static final long serialVersionUID = 1L;
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final String TITLE = "FirstGame 0.01";
+
+    private Thread thread;
+    private boolean running = false;
+
+    private void start() {
+        if (running) {
+            return;
+        }
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void stop() {
+        if (!running) {
+            return;
+        }
+        running = false;
+        try {
+            thread.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    public void run() {
+        while (running) {
+tick();
+render();
+        }
+    }
+
+    private void tick(){
+
+    }
+
+    private void render(){
+
+    }
+
+    public static void main(String[] args) {
+        Display game = new Display();
+        JFrame kurtisFrame = new JFrame();
+
+        kurtisFrame.pack();
+        kurtisFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        kurtisFrame.setLocationRelativeTo(null);
+        kurtisFrame.setTitle(TITLE);
+        kurtisFrame.setSize(WIDTH, HEIGHT);
+        kurtisFrame.add(game);
+        kurtisFrame.setResizable(false);
+        kurtisFrame.setVisible(true);
+
+        System.out.println("Running......");
+
+        game.start();
+    }
+}
